bfa4978d355532f2824ff8c52304178eabfc9a81
Joseph Bain <jbain359@gmail.com>
2021-10-15 17:51:19 -0400

    Maneki Mancer (In Development) Added

--- portfolio/src/GameDev/GameDev.js
+++ portfolio/src/GameDev/GameDev.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import screenshot from './Screenshots/SS1.PNG';
+import screenshot2 from './Screenshots/ManekiMancerMockup.png';
 import ProjectCard from '../Helpers/ProjectCard';
 
 const GameDev = () => {
   return (
     <>
       <div className="info">
-        <section className="projects-container">
+        <section className="game-projects-container">
           <h3>Projects</h3>
           <ul>
             <li>
@@ -14,11 +15,24 @@ const GameDev = () => {
                 class="game-project"
                 imgSource={screenshot}
                 title="Fort Mortem"
-                description="Strategic survival game  where you clear each level by coordinating your defense to outlast the enemy's offensive"
+                description="Strategic survival game where you clear each level by coordinating your defense to outlast the enemy's offensive"
                 links={[
                   {
                     name: 'Play Now',
                     href: 'https://haywirejo.itch.io/fort-mortem',
+                  },
+                ]}
+              ></ProjectCard>
+            </li>
+            <li>
+              <ProjectCard
+                class="game-project"
+                imgSource={screenshot2}
+                title="Maneki Mancer"
+                description="Arcade style platformer where you collect coins and use them to defeat enemies and advance through stages"
+                links={[
+                  {
+                    name: 'In Development',
                   },
                 ]}
               ></ProjectCard>
