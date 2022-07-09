7be31a8b1e7420f4991b60e253eededb12bdb8af
Joseph Bain <jbain359@bu.edu>
2022-07-09 00:12:43 -0400

    Added Night Slash Promo to Game Dev Page

--- portfolio/src/GameDev/GameDev.js
+++ portfolio/src/GameDev/GameDev.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import screenshot from './Screenshots/SS1.PNG';
 import screenshot2 from './Screenshots/MMScreenshotRoyalCatyard.PNG';
+import screenshot3 from './Screenshots/NightOfTheVamuraiMockup.png';
 import ProjectCard from '../Helpers/ProjectCard';
 
 const GameDev = () => {
@@ -15,7 +16,7 @@ const GameDev = () => {
                 class="game-project"
                 imgSource={screenshot}
                 title="Fort Mortem"
-                description="Strategic survival game where you clear each level by coordinating your defense to outlast the enemy's offensive"
+                description="Strategic survival game where you clear each level by coordinating your defense to outlast the enemy's offensive!"
                 links={[
                   {
                     name: 'Play Now',
@@ -29,11 +30,25 @@ const GameDev = () => {
                 class="game-project"
                 imgSource={screenshot2}
                 title="Maneki Mancer"
-                description="Arcade style platformer where you collect coins and use them to defeat enemies and advance through stages"
+                description="Arcade style platformer where you collect coins and use them to defeat enemies and advance through stages."
                 links={[
                   {
                     name: 'Play Now',
                     href: 'https://haywirejo.itch.io/maneki-mancer',
+                  },
+                ]}
+              ></ProjectCard>
+            </li>
+            <li>
+              <ProjectCard
+                class="game-project"
+                imgSource={screenshot3}
+                title="Night Slash"
+                description="Slash, clash, and dash 'em up as a Vampiric Samurai in the name of vengeance. Rally the undead and conquer the lands!"
+                links={[
+                  {
+                    name: 'Coming Soon',
+                    href: '',
                   },
                 ]}
               ></ProjectCard>
